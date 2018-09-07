cbaef9ec08a55163840bca3a914b6069e53434da
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-07 06:05:56 +0000

    update: Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Userモデルの中では右式のselfを省略できる
+  # before_save { self.email = email.downcase }
+  # before_save { self.email = self.email.downcase }
+  # 破壊的downcaseに変更
+  before_save { email.downcase! }
+  
+  # メソッドの最後の引数としてハッシュを渡す場合、波カッコを付けなくてもよい
+  # validates(:name, presence: true)
+  validates :name, presence: true,  length: { maximum: 50 }
+
+  # メールアドレスの正規表現
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,75 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # 各テストが走る直前に実行されるメソッド
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  # 有効なUserかどうかをテスト
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  # name属性の存在性の検証
+  test "name should be present" do
+    @user.name = "     "
+    # Userオブジェクトが有効でなくなったことを確認
+    assert_not @user.valid?
+  end
+
+  # email属性の存在性の検証
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  # name属性の長さの検証(51文字)
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  # email属性の長さの検証(255文字)
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  # email属性のフォーマットの検証(valid)
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # どのメールアドレスでテストが失敗したのかを特定できる
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # email属性のフォーマットの検証(Invalidity)
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  # email属性の一意性の検証
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  # email属性を小文字にする検証
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
