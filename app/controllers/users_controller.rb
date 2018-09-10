402fd49e14bf9fafdca69f005464ba780fe8b993
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-10 10:22:01 +0000

    add: Signup成功時のintegrationテストの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # 上記と等価
+      # redirect_to user_url(@user)
     else
       render 'new'
     end
   end
-  
+
   private
-  
+
     # paramsハッシュでは:user属性を必須とし、
     # 名前、メールアドレス、パスワード、パスワードの確認の属性をそれぞれ許可
     def user_params
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  # 失敗時テスト
   test "invalid signup information" do
     # getメソッドを使ってユーザー登録ページにアクセス
     get signup_path
@@ -16,12 +18,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           email: "user@invalid",
                                           password:              "foo",
                                           password_confirmation: "bar" } }
-      end
-      # newアクションが再描画されるかのテスト
-      # 非推奨では？
-      assert_template 'users/new'
-      # エラーメッセージのテスト
-      assert_select 'div#error_explanation'
-      assert_select 'div.field_with_errors'
+    end
+    # newアクションが再描画されるかのテスト
+    # 非推奨では？
+    assert_template 'users/new'
+    # エラーメッセージのテスト
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+    
+  # 成功時テスト
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    # POSTリクエストを送信した結果を見て、
+    # 指定されたリダイレクト先に移動するメソッド
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
