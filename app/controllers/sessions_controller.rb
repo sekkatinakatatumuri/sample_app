d25c64d0e9bb74d725707a915aa27b220c9e704f
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-11 14:52:44 +0000

    add: ログイン失敗時のflashメッセージとテストを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # ユーザーがデータベースにあり、かつ、認証に成功した場合
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
