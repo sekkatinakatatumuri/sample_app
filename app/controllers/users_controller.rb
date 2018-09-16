26f39babbbc16221ee54ad82295df5c799b22f81
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-16 15:05:35 +0000

    add: Userコントローラにeditとupdateアクション実行時はログインを要求する処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -36,10 +37,21 @@ class UsersController < ApplicationController
   end
 
   private
-
+    ## StrongParameters
+    
     # paramsハッシュでは:user属性を必須とし、
     # 名前、メールアドレス、パスワード、パスワードの確認の属性をそれぞれ許可
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+  ## BeforeAction
+  
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
