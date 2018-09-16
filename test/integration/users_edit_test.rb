da851bee3d055c82a98df10a86343fa95e38c030
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-16 13:46:05 +0000

    add: ユーザー編集成功時のintegrationテストを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  # ユーザ編集失敗時テスト
   test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
@@ -13,8 +14,28 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               email: "foo@invalid",
                                               password:              "foo",
                                               password_confirmation: "bar" } }
-
     assert_template 'users/edit'
     assert_select "div.alert", "The form contains 4 errors."
+  end
+  
+  # ユーザ編集成功時テスト
+  test "successful edit" do
+    # ユーザー情報を更新する正しい振る舞い
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    # flashメッセージが空でないか確認
+    assert_not flash.empty?
+    # プロフィールページにリダイレクトされるか確認
+    assert_redirected_to @user
+    # DB内のユーザー情報が正しく変更されたか確認
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
   end
 end
