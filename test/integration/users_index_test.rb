cfee5de8e95220be6ce211b6f6026f62b6ede4e1
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-18 14:04:38 +0000

    add: ユーザー一覧画面のintegrationテストを追加

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "index including pagination" do
+    # ログインする
+    log_in_as(@user)
+    # usersのindexページにアクセス
+    get users_path
+    # テンプレートの確認
+    assert_template 'users/index'
+    # ページネーションのリンクがあることを確認
+    assert_select 'div.pagination'
+    # 最初のページにユーザーがいることを確認
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+end
