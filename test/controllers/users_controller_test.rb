1b8d1d4d8be90ab06df1f8c22541c877fb502e79
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-18 12:32:16 +0000

    add: ユーザーの一覧ページを実装

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,6 +8,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  # indexアクションが正しくリダイレクトするか検証するテスト
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
