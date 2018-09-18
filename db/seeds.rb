bb6cb1939119f2bfb8c90cc8c0e9a59836e5c6d0
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-18 14:29:28 +0000

    update: seedsファイルのサンプルデータ生成タスクに管理者を1人追加するように更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,12 @@
 
 # create!は基本的にcreateメソッドと同じものですが、
 # ユーザーが無効な場合にfalseを返すのではなく例外を発生させる
+# admin: trueで管理者ユーザを作成
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
