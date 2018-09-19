62f879fb5a8f5f6b9fcc431e2ec6d1c937f0e428
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-19 14:20:20 +0000

    update: Userモデルのdowncase_emailプライベートメソッドをリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,8 @@ class User < ApplicationRecord
     
     # メールアドレスをすべて小文字にする
     def downcase_email
-      self.email = email.downcase
+      # self.email = email.downcase
+      self.email.downcase!
     end
     
     # 有効化トークンとダイジェストを作成および代入する
