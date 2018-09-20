b06ce64014f767027a4a8e8c91a96bb6e48ac93d
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-20 09:44:25 +0000

    update: Userメイラーのテストを更新

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  
+  # 送信メールのテスト
   test "account_activation" do
-    mail = UserMailer.account_activation
+    # fixtureユーザーに有効化トークンを追加
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    # 正規表現で文字列を確認
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    # エスケープ済みメールアドレスがメール本文に含まれているか確認
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
-
-  test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
-
 end
