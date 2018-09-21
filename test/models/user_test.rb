f018e6d697167aef405c51fdf252c3e2c5fa151c
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-21 12:22:27 +0000

    update: Userモデルテストのauthenticated?メソッドの実引数を2つに更新

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -93,6 +93,6 @@ class UserTest < ActiveSupport::TestCase
   
   # 記憶ダイジェストを持たないユーザーを用意し検証
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
