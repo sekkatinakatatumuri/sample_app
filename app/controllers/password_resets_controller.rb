35b754c26f0cd0582ce144af00c231fab2336656
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-25 08:55:11 +0000

    Userモデルにパスワード再設定用メソッドを追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,9 +7,9 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     # ユーザーが存在すれば
     if @user
-      # パスワード再設定用トークンと
-      # 送信時のタイムスタンプでデータベースの属性を更新
+      # パスワード再設定の属性を設定
       @user.create_reset_digest
+      # パスワード再設定のメールを送信
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
