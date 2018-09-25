0fbdd3ccfc2d4dd97850e57750a3cb1ea78b229d
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-25 10:01:11 +0000

    add: パスワード再設定のフォームが出力するため、パスワード再設定のeditアクションを実装

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -21,4 +24,19 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+    private
+    
+    # params[:email]のメールアドレスに対応するユーザーを取得
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+    
+    # ユーザーが存在する&有効化されている&認証済みであることを確認
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
