c70ef99f0e6107e214ea2a0b85702bd5be1afaa2
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-09-28 11:36:05 +0000

    add: Micropostモデルにpictureカラムを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   # データベースから要素を取得したときの、デフォルトの順序を指定するメソッド
   default_scope -> { order(created_at: :desc) }
+  # CarrierWaveに画像と関連付けたモデルを伝える
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
