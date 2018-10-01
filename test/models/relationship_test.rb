31336eeb3c3675f5340aaacb7af386c1c9bf4d03
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-01 09:20:42 +0000

    add: Relationshipモデルテストにバリデーションをテストするコードを追加

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id,
+                                     followed_id: users(:archer).id)
+  end
+
+  # Relationshipモデルインスタンスのバリデーションをテスト
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  # follower_idが必須であることをテスト
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  # followed_idが必須であることをテスト
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
