285ccd9f30dedfff609e63f84dd67fb06a59e754
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-01 16:19:43 +0000

    add: Userモデルにfollowingの関連付け(has_many)を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                 foreign_key: "follower_id",
                                 dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   # Userモデルの中では右式のselfを省略できる
