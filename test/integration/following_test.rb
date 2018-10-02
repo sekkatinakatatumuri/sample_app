dbbe0b8d5b51b757ad1b38204abbf7e2d581a1d3
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-02 13:35:12 +0000

    add: Relationshipsコントローラーを生成

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class FollowingTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+
+  # フォロー画面のテスト
+  test "following page" do
+    get following_user_path(@user)
+    assert_not @user.following.empty?
+    # 正しい数かどうかを確認
+    assert_match @user.following.count.to_s, response.body
+    @user.following.each do |user|
+      # 正しいURLかどうかを確認
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+  
+  # フォロワー画面のテスト
+  test "followers page" do
+    get followers_user_path(@user)
+    assert_not @user.followers.empty?
+    assert_match @user.followers.count.to_s, response.body
+    @user.followers.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
 end
