e5342fda88fd837e747ec2c8cb44c8ffc2f826d8
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-02 13:49:17 +0000

    add: Relationshipsコントローラーのcreate, destroyアクションを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,19 @@
 class RelationshipsController < ApplicationController
+  # リレーションシップのアクセス制御
+  before_action :logged_in_user
+
+  def create
+    # followed_idに対応するユーザーを取得
+    user = User.find(params[:followed_id])
+    # 取得したユーザーに対してfollowメソッドを実行
+    current_user.follow(user)
+    # 元のプロフィールにリダイレクト
+    redirect_to user
+  end
+  
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
 end
--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # RelationshipsコントローラーのCreateアクションのテスト
+  test "create should require logged-in user" do
+    # Relationshipのカウントが変わっていないことを確認
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    # ログインページにリダイレクトされることを確認
+    assert_redirected_to login_url
+  end
+
+  # Relationshipsコントローラーのdestroyアクションのテスト
+  test "destroy should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
