52be4c45177893ecfdc967cb93de1bcccc106427
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-22 20:22:37 +0900

    add: Gemファイルにpry-railsとpry-byebugを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -36,9 +36,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }
     end
     # 配信されたメッセージが1つであるかどうかを確認
     assert_equal 1, ActionMailer::Base.deliveries.size
