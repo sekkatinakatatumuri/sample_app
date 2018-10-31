5f95323c3b89089f33dcacf8442614bab8b60a41
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-10-31 17:36:50 +0900

    update: prodctionコンフィグのSendGridのhost部分を更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Railsのproduction環境でSendGridを使う設定
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'intense-crag-48325.herokuapp.com'
+  host = 'sampleapp-tfujii.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
